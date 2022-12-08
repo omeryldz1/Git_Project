6275fcf970fd9cc50116c9a2627934a6a215cde1
Ömer Yıldız <omergsyildiz@gmail.com>
2022-12-08 18:37:04 +0300

    created test case 001

---
+++ src/day1/TestCase001.java
@@ -0,0 +1,32 @@
+package day1;
+
+public class TestCase001 {
+    // test case 001
+
+    // test login
+
+    // using valid credentials
+
+    // testttttt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
